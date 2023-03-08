1edaddfec4c592523fc745ece391f531c8a15309
Veronica Berglyd Olsen <1619840+vkbo@users.noreply.github.com>
2023-03-08 17:26:08 +0100

    Add generated Sphinx content

---
+++ conf.py
@@ -0,0 +1,27 @@
+# Configuration file for Sphinx
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Veronica Writes'
+copyright = '2023, Veronica Berglyd Olsen'
+author = 'Veronica Berglyd Olsen'
+
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
